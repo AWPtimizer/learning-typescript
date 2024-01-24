c535f5959961d531c8acb156e08b078ff39170f0
AWPtimizer <luhemdha@gmail.com>
2024-01-24 17:00:55 +0530

    Learned about Narrowing and in operator

---
+++ 10-generics/src/detection.ts
@@ -0,0 +1,48 @@
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("Please provide id");
+    return;
+  }
+  id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+  // !!!!!!!!!!!!!!!!
+  //  DON'T DO THIS!
+  //   KEEP READING
+  // !!!!!!!!!!!!!!!!
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
+
+// operator narrowing i.e. in operator =>
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}

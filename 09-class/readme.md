62d07e54aa235ce406dab5661c37aba36819821a
AWPtimizer <luhemdha@gmail.com>
2024-01-24 13:15:31 +0530

    Learned Getters and Setters

--- 09-class/readme.md
+++ 09-class/readme.md
@@ -1,2 +1,15 @@
 Initialize a tsconfig.json file first by ->  "tsc --init" command
 dist means distributions
+
+We cannot have a return type in a setter ->
+i.e.
+You don't have to write a return type bcz "A 'set' accessor cannot have a return type annotation"
+
+  set courseCount(courseNumber): void {
+    
+  }
+
+To gix this remove the return type->
+  set courseCount(courseNumber) {
+
+  }
--- 09-class/src/index.ts
+++ 09-class/src/index.ts
@@ -11,8 +11,7 @@
 // const Mehul = new User("m@m.com", "raju");
 // Mehul.city = "Udaipur";
 
-
-// private and public class -> 
+// private and public class ->
 // class User {
 //   public email: string;
 //   private name: string;
@@ -28,12 +27,53 @@
 
 // how we use these classes in production ->
 
+// class User {
+//   constructor(
+//     public email: string,
+//     public name: string,
+//     // private userId: string,
+//     ) {
+//   }
+// }
+
+// const Mehul = new User("m@m.com", "raju");
+
+
+// getters and setters ->
 class User {
+
+  private _courseCount = 1;
+
+  readonly city: string = "Udaipur";
+
   constructor(
-    public email: string, 
-    public name: string,
-    // private userId: string,
-    ) {
+    public email: string,
+    public name: string
+  ) // private userId: string,
+  {
+    this.deleteToken()
+  }
+
+  // private methods ->
+  private deleteToken() {
+    console.log("Token Deleted");
+  }
+
+  // get and set ->
+
+  get getAppleEmail(): string {
+    return `apple.${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNumber) {
+    if(courseNumber <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNumber
   }
 }
 

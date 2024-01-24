d09f845e85d3110ca5d39f0eade976788e38a9e2
AWPtimizer <luhemdha@gmail.com>
2024-01-24 16:23:59 +0530

    Learned about how we use Generics class

--- 10-generics/src/generics.ts
+++ 10-generics/src/generics.ts
@@ -9,13 +9,13 @@ function identityTwo(val: any): any {
   return val;
 }
 
-function identityThree<Type,>(val: Type): Type {
+function identityThree<Type>(val: Type): Type {
   return val;
 }
 // identityThree(true);
 
 // better way to define the 3rd function =>
-function identityThreeBetter<T,>(val: T): T {
+function identityThreeBetter<T>(val: T): T {
   return val;
 }
 
@@ -29,14 +29,52 @@ identityThreeBetter<Bottle>({
 });
 
 // Generics in Arrays and Arrow functions =>
-
-function getSearchProducts<T,>(products: T[]): T {
+function getSearchProducts<T>(products: T[]): T {
   // do some database operations
   const myIndex = 3;
   return products[myIndex];
 }
 
-const getMoreSearchProducts = <T,>(products: T[]): T=> {
+const getMoreSearchProducts = <T>(products: T[]): T => {
   const myIndex = 7;
   return products[myIndex];
+};
+
+// generics classes =>
+interface DB {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends DB>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+anotherFunction("3", {
+  connection: "saf",
+  username: "sad",
+  password: "sfdaf",
+});
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = []
+
+  addToCart(product: T) {
+    this.cart.push(product)
+  }
 }

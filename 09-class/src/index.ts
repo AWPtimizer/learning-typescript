ba9a6daaa4410d42d404154f57fbab5c997358c4
AWPtimizer <luhemdha@gmail.com>
2024-01-24 13:00:23 +0530

    Added how to use public and private class

--- 09-class/src/index.ts
+++ 09-class/src/index.ts
@@ -1,12 +1,40 @@
+// class User {
+//   email: string;
+//   name: string;
+//   city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// const Mehul = new User("m@m.com", "raju");
+// Mehul.city = "Udaipur";
+
+
+// private and public class -> 
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city = name
+//   }
+// }
+
+// const Mehul = new User("m@m.com", "raju");
+
+// how we use these classes in production ->
+
 class User {
-  email: string;
-  name: string;
-  city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  constructor(
+    public email: string, 
+    public name: string,
+    // private userId: string,
+    ) {
   }
 }
 
 const Mehul = new User("m@m.com", "raju");
-Mehul.city = "Udaipur";
